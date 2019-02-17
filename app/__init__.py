3ce6167a7dfe463361fc5ca8f32a36f99213912b
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2019-02-17 03:58:52 +0000

    Final

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 't00muc4'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465' # (or try 2525)
-app.config['MAIL_USERNAME'] = '63107dcc396033'
-app.config['MAIL_PASSWORD'] = '57708637782c99'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 from app import views
